1b0c0ef206c15498e5f5d5e2c441caba4d1bcfc1
nohaderf <freda.hon@gmail.com>
2020-11-15 23:58:07 -0500

    commit tree 86d7d8877f8be1f1bdc72ab50c26d492d210d59f

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10 ) + 1
+
+const mod = 28 % 6
+
+const max = Math.max(1,2,14,20,17)
